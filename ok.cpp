f3a189ea19f9c56b1a7208257b9da5827cc41937
root-kush369 <techkush777@gmail.com>
2025-03-13 23:47:59 +0530

    created recursion by kush-369 as said by KM-Bro ,close #2

--- ok.cpp
+++ ok.cpp
@@ -1,7 +1,47 @@
-#include <iosteam>
-int main()
-{
- cout << "written from KM account" ;
+#include <iostream>
+using namespace std;
 
-  return 0;
+//recursion means n number of runtimecreation in address_Space
+void printSequence(int n , int x , int first,int second,int temp)
+{
+	
+	
+	if(first ==0 && second==1)
+		cout<<first<<" "<<second<<"  " ;
+
+/*		temp=first+second;
+		cout<<temp<<" " ;
+		first=second;
+		second=temp; */
+					
+
+	if(n< x-(2) ) //recursion break
+	{
+		return ;
+	}
+//	else 
+//	{	
+		temp=first+second;
+		first=second;
+		second=temp;
+
+		cout<<temp<<" " ;
+		printSequence(n-1,5,first,second,temp); //recursion call function
+//	}
+}
+int main(){
+
+
+	int input = 0 , x=0 , temp=0 ,first=0,second=1;
+	
+	cout<<"enter input : " ;
+
+	cin >> input ;
+	
+	temp=input ;
+	x=input;
+
+	printSequence(input ,x, first,second,temp);
+
+	return 0 ; 
 }
